5e45813d1ee868150e393b555af0487cedd63e87
Henry Gustavo X Az <azhenrygus@outlook.es>
2023-11-19 22:42:56 -0600

    add layouts: guest and app

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{config("app.name")  }}</title>
+
+    @vite(['resources/css/app.css'])
+</head>
+<body>
+    <h1>app layout</h1>
+    <main>
+        {{ $slot }}
+    </main>
+    @vite(['resources/js/app.js'])
+</body>
+</html>

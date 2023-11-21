4b521f6b252e594fdf02e9f8ac438950a725fbe7
Henry Gustavo X Az <azhenrygus@outlook.es>
2023-11-20 23:40:38 -0600

    add main structure and styles to guest layout

--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -8,8 +8,11 @@
 
     @vite(['resources/css/app.css'])
 </head>
-<body>
-    <main>
+<body class="bg-light-color-2 dark:bg-dark-color-2">
+    <div class="max-w-7xl mx-auto mt-5 flex justify-end">
+        <x-toggle-theme.toggle-theme />
+    </div>
+    <main class="my-6 mx-auto container">
         {{ $slot }}
     </main>
     @vite(['resources/js/app.js'])

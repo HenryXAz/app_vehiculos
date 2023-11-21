85573a42d6e252248cbcfafb381c9244dd6bc866
Henry Gustavo X Az <azhenrygus@outlook.es>
2023-11-20 23:35:13 -0600

    enable dark mode with class and custom colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,20 @@ export default {
     './resources/**/*.vue',
     './resources/**/*.js'
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          'color-1': '#212529',
+          'color-2': '#2B3035',
+        },
+        light: {
+          'color-1': '#F8F9FA',
+          'color-2': '#E9ECEF',
+        }
+      }
+    },
   },
   plugins: [],
 }

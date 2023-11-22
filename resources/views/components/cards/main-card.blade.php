844fc802c203f957286be879643125fe94f6ac69
Henry Gustavo X Az <azhenrygus@outlook.es>
2023-11-21 22:43:56 -0600

    add class prop to main card

--- resources/views/components/cards/main-card.blade.php
+++ resources/views/components/cards/main-card.blade.php
@@ -1,5 +1,7 @@
+@props(['class' => ""])
+
 <div 
-class="w-full mx-auto my-5 bg-light-color-1 dark:bg-dark-color-1 shadow-md p-5 rounded-md"
+class="{{$class . " w-full mx-auto my-5 bg-light-color-1 dark:bg-dark-color-2 shadow-md p-5 rounded-lg"}}"
 >
     {{$slot}}
 </div>

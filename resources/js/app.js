b8741368bad2f550f8823745bc2e89e9ac2d31f7
Henry Gustavo X Az <azhenrygus@outlook.es>
2023-11-21 22:32:27 -0600

    add alpinejs in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
+import userCardSidebar from './alpine-components/user-card-sidebar';
 import './bootstrap';
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine
+
+Alpine.data('user_card_sidebar', userCardSidebar)
+
+Alpine.start()

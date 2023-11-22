78d9752724049d5771046ea6b3d9af471f0af3ef
Henry Gustavo X Az <azhenrygus@outlook.es>
2023-11-21 22:44:40 -0600

    add user card sidebar

---
+++ resources/views/components/user-card-sidebar/user-card-sidebar.blade.php
@@ -0,0 +1,38 @@
+<nav class="w-full my-10" x-data="user_card_sidebar">
+    <button class="border rounded-md border-gray-300 dark:border-gray-700 p-2 wire:bg-transparent outline-none flex justify-between w-full hover:cursor-pointer"
+        @click="openOptions"
+    >
+        <p class="text-sm dark:text-gray-200 text-gray-800">
+            user@example.com
+        </p>
+            <span class="font-light text-gray-600 dark:text-gray-200"
+            x-show="!showOptions"
+        >
+            ▼
+        </span>
+
+        <span class="font-light text-gray-600 dark:text-gray-200"
+            x-show="showOptions"
+        >
+            ▲
+        </span>
+
+    </button>
+
+    <ul class="w-full flex flex-col border-b border-b-gray-300 dark:border-b-gray-600  rounded-md"
+        x-show="showOptions" x-transition.duration.300ms
+    >
+        <li>
+            <x-user-card-sidebar.link href="#">
+                Perfil
+            </x-user-card-sidebar.link>
+        </li>
+        <li>
+            <x-form.form action="action.php">
+                <x-user-card-sidebar.link type="submit">
+                    Cerrar Sesión
+                </x-user-card-sidebar.link>
+            </x-form.form>
+        </li>
+    </ul>
+</nav>

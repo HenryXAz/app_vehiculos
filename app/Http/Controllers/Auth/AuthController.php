72c764e073a8c364f41eb6dbb2d5ae3257062dad
Henry Gustavo X Az <azhenrygus@outlook.es>
2023-12-21 23:47:57 -0600

    add loginDTO to login AuthController method

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Entities\Auth\UserCredentials;
+use App\Dtos\Auth\LoginDTO;
 use App\Exceptions\Auth\AuthException;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\AuthRequest;
 use App\Services\AuthService\AuthService;
 use Illuminate\Http\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AuthController extends Controller
 {
@@ -19,23 +20,26 @@ class AuthController extends Controller
     public function login(AuthRequest $request): RedirectResponse
     {
         try {
-            $credentials = new UserCredentials(
-                email: $request->email,
-                password: $request->password,
-                remember: ($request->has('remember') ? $request->remember : false),
-            );
+            $data = json_decode(json_encode($request->only(['email', 'password', 'remember'])));
+            $credentials = LoginDTO::makeLoginDTO($data);
 
             return $this->authService->login($credentials);
-        } catch (AuthException $th) {
+        } catch (\Throwable $th) {
             if($th instanceof AuthException) {
                 return redirect(route("login"))->withErrors([
                     "error_login" => $th->getMessage(),
                 ]);
             }
 
-            return redirect("login")->withErrors([
-                "error_login" => "500 internal server error"
-            ]);
+            if($th instanceof NotFoundHttpException )
+            {
+                return redirect(route("login"))->withErrors([
+                    "error_login" => $th->getMessage(),
+                ]);
+            }
+
+            dd($th->getMessage());
+            // abort(500);
         }
     }
 

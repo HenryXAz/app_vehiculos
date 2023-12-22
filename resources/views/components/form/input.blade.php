5922db1ca63b687149abfa38ca97a83e4c6a0f8a
Henry Gustavo X Az <azhenrygus@outlook.es>
2023-12-21 23:43:28 -0600

    add value property to input component and new classes

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,8 +1,9 @@
-@props(['type' => 'text', "name" => "", "id" => ""])
+@props(['type' => 'text', "name" => "", "id" => "", "value" => ""])
 
 <input type="{{$type}}"
     name="{{$name}}"
     id="{{$id}}"
-    class="p-1.5 font-light text-sm md:text-base bg-transparent text-gray-900 dark:text-gray-200 outline-none
+    value="{{$value}}"
+    class="w-full p-1.5 font-light text-sm md:text-base bg-transparent text-gray-900 dark:text-gray-200 outline-none
     border border-gray-400 dark:border-gray-500 rounded-md  focus:outline-4 focus:outline-blue-400 dark:focus:outline-blue-500"
 >
